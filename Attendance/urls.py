01ae91166493849c94ef182cc5d7c96b7e2fe9dd
Clark Perkins <r.clark.perkins@gmail.com>
2014-02-11 01:41:35 -0600

    created homepage

---
+++ Attendance/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+from Attendance import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='home'),
+)
--- sinfoniaattendance/urls.py
+++ sinfoniaattendance/urls.py
@@ -9,5 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^/', include('Attendance.urls')),
+    url(r'^', include('Attendance.urls'), name='attendance'),
 )
